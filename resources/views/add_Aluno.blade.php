fb43aab74fa21538c54c86f379ec532cfd460a0c
JeffersonLop1201 <jeffersonlopes1201@gmail.com>
2024-05-16 21:19:37 -0300

    add aluno

---
+++ resources/views/add_Aluno.blade.php
@@ -0,0 +1,25 @@
+<div id="addmodal" class="addmodal">
+    <div class="addmodal-content">
+        <span class="addmodal-close">&times;</span>
+        <h2>Adicionar Usuário</h2>
+        <form id="add-user-form">
+            <input type="text" id="add-user-name" placeholder="Nome do usuário" required>
+            <input type="email" id="add-user-email" placeholder="E-mail do usuário" required>
+            <input type="tel" id="add-user-phone" placeholder="Telefone do usuário" required>
+            <select id="add-user-icon">
+                <option value="👤">👤</option>
+                <option value="👩">👩</option>
+                <option value="👨">👨</option>
+                <option value="👧">👧</option>
+                <option value="🧑">🧑</option>
+                <option value="🧒">🧒</option>
+                <option value="👨">👨</option>
+                <option value="🧔">🧔</option>
+                <option value="👨‍🦱">👨‍🦱</option>
+                <option value="👱">👱</option>
+                <option value="👱‍♀️">👱‍♀️</option>
+            </select>
+            <button type="submit" class="btn-submit">Adicionar</button>
+        </form>
+    </div>
+</div>
