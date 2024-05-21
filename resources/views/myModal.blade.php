2e0b95d4f8b66be170eea5c242b20f4f9f231203
jeffersonLop1201 <jeffersonlopes1201@gmail.com>
2024-05-20 22:28:02 -0300

    oi

--- resources/views/myModal.blade.php
+++ resources/views/myModal.blade.php
@@ -8,8 +8,6 @@
         <div class="navbar">
             <a href="#" class="nav-item" id="btn-dados"
                 onclick="toggleModalActive('dados'); toggleNavActive('btn-dados')">Dados</a>
-            <a href="#" class="nav-item" id="btn-carteirinha"
-                onclick="toggleModalActive('carteirinha'); toggleNavActive('btn-carteirinha')">Carteirinha</a>
             <a href="#" class="nav-item" id="btn-chat"
                 onclick="toggleModalActive('chat'); toggleNavActive('btn-chat')">Chat</a>
             <a href="#" class="nav-item" id="btn-responsavel"
@@ -17,80 +15,90 @@
             <div class="line"></div>
         </div>
         <div class="modal-screen active cont-modal-inicio" id="inicio">
-
+            Bem vindo
         </div>
         <div class="modal-screen cont-modal-user-dados" id="dados">
-            <div class="cont-dados">
-                <div class="cont-0">
-                    <h3>Id:</h3>
+        <div class="cont-dados"> 
+                <div class="cont-0">  
+                    <h3>ID:</h3>
                     <div class="cont-id">
                         <p>23047</p>
                     </div>
                 </div>
-                <div class="cont-1">
+                <div class="cont-1 oi">
                     <h3>nome:</h3>
                     <div class="cont-nome">
                         <p>Jefferson Lopes de Sousa</p>
                     </div>
                 </div>
-                <div class="cont-2">
+                <div class="cont-2 oi">
                     <h3>Email:</h3>
                     <div class="cont-email">
                         <p>jeffersonlopes1201@gmail.com</p>
                     </div>
                 </div>
-                <div class="cont-3">
+                <div class="cont-3 oi">
                     <h3>Data de Nascimento:</h3>
                     <div class="cont-dtNasc">
                         <p>12/01/2007</p>
                     </div>
                 </div>
-                <div class="cont-4">
+                <div class="cont-4 oi">
                     <h3>Genero:</h3>
                     <div class="cont-genero">
                         <p>Masculino</p>
                     </div>
                 </div>
-                <div class="cont-5">
+                <div class="cont-5 oi">
                     <h3>Telefone:</h3>
                     <div class="cont-telefone">
                         <p>(11)98451-6598</p>
                     </div>
                 </div>
-                <div class="cont-6">
+                <div class="cont-6 oi">
                     <h3>curço/Série/Ensino:</h3>
                     <div class="cont-Cse">
                         <p>Técnico em Desenvolvimento de Sistemas</p>
                     </div>
                 </div>
-                <div class="cont-7">
+                <div class="cont-7 oi">
                     <h3>Instituição:</h3>
                     <div class="cont-instituição">
                         <p>Escola Técnica Estadual de Guarulhos</p>
                     </div>
                 </div>
-                <div class="cont-8">
+                <div class="cont-8 oi">
                     <h3>Pessoa com Deficiencia:</h3>
                     <div class="cont-nome">
                         <p>não</p>
                     </div>
                 </div>
-                <div class="cont-9">
+                <div class="cont-9 oi">
                     <h3>RG:</h3>
                     <div class="cont-nome">
                         <p>63.473.357-7</p>
                     </div>
                 </div>
-                <div class="cont-10">
+                <div class="cont-10 oi">
                     <h3>CPF:</h3>
                     <div class="cont-nome">
                         <p>588.546.349.35</p>
                     </div>
                 </div>
+                <div class="cont-10 oi">
+                    <h3>Carteirinha:</h3>
+                    <div class="cont-carteirinha">
+                        <div class="card-cont">
+                <div class="card-all">
+                <div class="card"></div>
+                <div class="fund-1"></div>
+                <div class="fund-2"></div>
+                <div class="fund-3"></div>
+                    </div>
+                </div>
+                </div>
             </div>
-        </div>
-        <div class="modal-screen cont-modal-user-carteirinha" id="carteirinha">
-
+            </div>
         </div>
         <!-- chat -->
         <div class="modal-screen cont-modal-user-chat" id="chat">
@@ -98,7 +106,44 @@
         </div>
         <!-- fim chat -->
         <div class="modal-screen cont-modal-user-responsavel" id="responsavel">
-
+        <div class="cont-dados"> 
+                <div class="cont-0">  
+                    <h3>ID:</h3>
+                    <div class="cont-id">
+                        <p>23047-25</p>
+                    </div>
+                </div>
+                <div class="cont-1 oi">
+                    <h3>Nome:</h3>
+                    <div class="cont-nome">
+                        <p>Manuela Monte de Sousa</p>
+                    </div>
+                </div>
+                <div class="cont-2 oi">
+                    <h3>Email:</h3>
+                    <div class="cont-email">
+                        <p>mannu131@live.com</p>
+                    </div>
+                </div>
+                <div class="cont-5 oi">
+                    <h3>Telefone:</h3>
+                    <div class="cont-telefone">
+                        <p>(11)98451-6598</p>
+                    </div>
+                </div>
+                <div class="cont-9 oi">
+                    <h3>RG:</h3>
+                    <div class="cont-nome">
+                        <p>63.473.357-7</p>
+                    </div>
+                </div>
+                <div class="cont-10 oi">
+                    <h3>CPF:</h3>
+                    <div class="cont-nome">
+                        <p>588.546.349.35</p>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
